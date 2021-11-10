6fec81f08f856e4dace1a37a8a589677f758daa7
Filipe Tavares-Cadete <filipe@tavares.cadete.eu>
2021-11-10 23:13:59 +0000

    Removed hardcoded workflow and step IDs

--- main.R
+++ main.R
@@ -3,9 +3,6 @@ library(dplyr)
 library(DESeq2)
 library(tidyr)
 
-options("tercen.workflowId" = "e20dce0618dceee40baff0f67f003513")
-options("tercen.stepId"     = "58c046df-2126-49f0-b498-2c2a5d61dc5c")
-
 getOption("tercen.workflowId")
 getOption("tercen.stepId")
 
