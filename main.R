2f8754fd970e474e8505aa84a9e55376f19d6353
agouy <contact@agouy.com>
2023-02-15 19:37:23 +0000

    refactoring for performance and ci + ri type fix for tercen 0.10

--- main.R
+++ main.R
@@ -1,10 +1,9 @@
-library(tercen)
-library(dplyr)
-library(DESeq2)
-library(tidyr)
-
-getOption("tercen.workflowId")
-getOption("tercen.stepId")
+suppressPackageStartupMessages({
+  library(tercen)
+  library(dplyr)
+  library(DESeq2)
+  library(tidyr)
+})
 
 ctx = tercenCtx()
 
@@ -13,26 +12,32 @@ if(length(ctx$rnames) == 0) stop("row information is missing.")
 if(length(ctx$cnames) == 0) stop("column information is missing.")
 if(length(ctx$colors) == 0) stop("color information is missing.")
 
-blind <- as.logical(ctx$op.value('blind'))
+blind <- ctx$op.value('blind', as.logical, FALSE)
 
+all_data <- ctx %>% select(.y, .ci, .ri, .colorLevels)
 
-all_data <- ctx %>% select(.ci, .ri,.colorLevels)
-
-count_matrix <- ctx$as.matrix()
+count_matrix <- all_data %>%
+  select(.y, .ri, .ci) %>%
+  pivot_wider(names_from = ".ci", values_from = ".y") %>%
+  select(-.ri) %>%
+  as.matrix()
 
 colData <- filter(all_data, .ri == 0) %>%
-  select(.ci, condition = .colorLevels)
+  select(.ci, condition = .colorLevels) %>%
+  mutate(condition = as.factor(condition))
 
-dds <- DESeqDataSetFromMatrix(countData = count_matrix,
-                              colData = colData,
-                              design = ~ condition)
+dds <- DESeqDataSetFromMatrix(
+  countData = count_matrix,
+  colData = colData,
+  design = ~ condition
+)
 
-vsd <- assay(vst(dds, blind=blind))
+vsd <- assay(varianceStabilizingTransformation(dds, blind = blind))
 
-vsd <- as_tibble(vsd) %>%
-  mutate(.ri = row_number() - 1) %>%
+df_out <- as_tibble(vsd) %>%
+  mutate(.ri = row_number() - 1L) %>%
   pivot_longer(-.ri, names_to = ".ci", values_to = "vsd") %>%
-  mutate(.ci = as.integer(.ci) - 1)
+  mutate(.ci = as.integer(.ci)) %>%
+  ctx$addNamespace()
 
-ctx$addNamespace(vsd) %>%
-  ctx$save()
+ctx$save(df_out)
